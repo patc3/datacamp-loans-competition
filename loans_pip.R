29be9e33c072d8175a0fd052f9ce966d8e422275
Patrick Coulombe <patrick.coulombe@d22consulting.com>
2021-08-27 01:17:55 +0000

    Add str() and summary() and comments in pip

--- loans_pip.R
+++ loans_pip.R
@@ -7,3 +7,9 @@ loans pip
 "
 source("loans_fn.R")
 df <- load_data(as_df = TRUE)
+str(df)
+summary(df)
+
+# could do gower and predict, but there's neither missing data and almost all vars are numeric!
+# could do pipelines
+# could do both
