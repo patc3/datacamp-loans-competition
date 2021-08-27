edfb2aa145ad1904282a8e5f86e5bf6673100bef
Patrick Coulombe <patrick.coulombe@d22consulting.com>
2021-08-27 01:13:45 +0000

    Add fn and pip templates with load_data()

---
+++ loans_fn.R
@@ -0,0 +1,16 @@
+"
+2021.08.26
+DataCamp competition
+
+loans fn
+
+"
+
+#### load data ####
+load_data <- function(as_df=FALSE)
+{
+  df <- readr::read_csv("data/loans.csv.gz")
+  if(as_df) class(df) <- "data.frame"
+  return(df)
+}
+
---
+++ loans_pip.R
@@ -0,0 +1,9 @@
+"
+2021.08.26
+DataCamp competition
+
+loans pip
+
+"
+source("loans_fn.R")
+df <- load_data(as_df = TRUE)
